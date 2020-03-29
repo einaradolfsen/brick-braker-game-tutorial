a275816b42490fac124d272afcf4db7e2f7d3392
einar adolfsen <einaradolfsen@lyse.net>
2020-03-29 21:03:29 +0200

    collision between ball and paddle

--- src/ball.js
+++ src/ball.js
@@ -3,6 +3,7 @@ export default class Ball {
     this.gameHeight = game.gameHeight;
     this.gameWidth = game.gameWidth;
     this.image = document.getElementById("img-ball");
+    this.game = game;
     this.speed = { x: 2, y: 2 };
     this.position = {
       x: 10,
@@ -25,9 +26,31 @@ export default class Ball {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
 
-    if (this.position.x + this.size >= this.gameWidth || this.position.x <= 0)
+    if (
+      this.position.x + this.size >= this.game.gameWidth ||
+      this.position.x <= 0
+    )
       this.speed.x = -this.speed.x;
-    if (this.position.y + this.size >= this.gameHeight || this.position.y <= 0)
+
+    if (
+      this.position.y + this.size >= this.game.gameHeight ||
+      this.position.y <= 0
+    )
       this.speed.y = -this.speed.y;
+
+    //check collision width paddle
+    let bottomBall = this.position.y + this.size;
+    let topOfPaddle = this.game.paddle.position.y;
+    let leftSideOfPaddle = this.game.paddle.position.x;
+    let rigthSideOfPaddle =
+      this.game.paddle.position.x + this.game.paddle.width;
+    if (
+      bottomBall >= topOfPaddle &&
+      this.position.x >= leftSideOfPaddle &&
+      this.position.x + this.size <= rigthSideOfPaddle
+    ) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
+    }
   }
 }
--- src/input.js
+++ src/input.js
@@ -14,12 +14,12 @@ export class InputHandler {
     });
 
     document.addEventListener("keyup", function(event) {
-      if (paddle.speed <= 0) return;
-
       switch (event.keyCode) {
         case 37:
+          if (paddle.speed < 0) paddle.stop();
+          break;
         case 39:
-          paddle.stop();
+          if (paddle.speed > 0) paddle.stop();
           break;
       }
     });
