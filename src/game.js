5fb975b3ccd7ebc015f583361bf23704b5e10dfd
einar adolfsen <einaradolfsen@lyse.net>
2020-03-29 21:48:26 +0200

    loading bricks

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import { Paddle } from "./paddle.js";
 import { InputHandler } from "./input.js";
 import Ball from "./ball.js";
+import Brick from "./brick.js";
 
 export default class Game {
   constructor(gameWidth, gameHeight) {
@@ -11,8 +12,12 @@ export default class Game {
   start() {
     this.paddle = new Paddle(this);
     this.ball = new Ball(this);
+    this.bricks = [];
+    for (var i = 0; i < 10; i++) {
+      this.bricks.push(new Brick(this, { x: i * 52, y: 10 }));
+    }
 
-    this.gameObjects = [this.paddle, this.ball];
+    this.gameObjects = [this.paddle, this.ball, ...this.bricks];
     new InputHandler(this.paddle);
   }
 
